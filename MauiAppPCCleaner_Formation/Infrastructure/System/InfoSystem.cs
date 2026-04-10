ce4fe55eda19ceabfc35c238d480190e2c7fd04c
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-04-10 13:06:57 +0200

    mise en place de la fenêtre princiaple

--- MauiAppPCCleaner_Formation/Infrastructure/System/InfoSystem.cs
+++ MauiAppPCCleaner_Formation/Infrastructure/System/InfoSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Management;
 using System.Text;
 using System.Runtime.Versioning;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MauiAppPCCleaner_Formation.Infrastructure.System
 {
@@ -14,30 +15,84 @@ namespace MauiAppPCCleaner_Formation.Infrastructure.System
         public string? Gpu { get; set; }
         #endregion
 
-        [SupportedOSPlatform("windows")]
-        public void DownloadSystemInfo()
+
+        public InfoSystem()
         {
             if (OperatingSystem.IsWindows())
             {
-                // Platform not supported: provide safe fallback values
-                Os = Environment.OSVersion.ToString();
-                Cpu = "Unavailable on this platform";
-                Gpu = "Unavailable on this platform";
-                return;
+                DownloadSystemInfo();
+            }
+        }
+
+
+        #region private methods
+        [SupportedOSPlatform("windows")]
+        private void DownloadSystemInfo()
+        {
+            ManagementObjectSearcher osObject = new ("SELECT * FROM Win32_OperatingSystem");
+            
+            foreach (ManagementObject mo in osObject.Get())
+            {
+                Console.WriteLine("Name - {0}", mo["Name"]);
+                
+                Os = mo["Name"]?.ToString()?.Split('|')[0] ?? string.Empty;
+
+            }
+            ManagementObjectSearcher cpuObject = new ("select * from Win32_Processor");
+            foreach (ManagementObject mo in cpuObject.Get())
+            {
+                Cpu = mo["Name"].ToString();
+                /*Console.WriteLine("Name - {0}", mo["Name"]);
+                Console.WriteLine("Status - {0}", mo["Status"]);
+                Console.WriteLine("Caption - {0}", mo["Caption"]);
+                Console.WriteLine("DeviceID - {0}", mo["DeviceID"]);
+                Console.WriteLine("AdapterRAM - {0}", mo["AdapterRAM"]);
+                Console.WriteLine("AdapterDACType - {0}", mo["AdapterDACType"]);
+                Console.WriteLine("Monochrome - {0}", mo["Monochrome"]);
+                Console.WriteLine("InstalledDisplayDrivers - {0}", mo["InstalledDisplayDrivers"]);
+                Console.WriteLine("DriverVersion - {0}", mo["DriverVersion"]);
+                Console.WriteLine("VideoProcessor - {0}", mo["VideoProcessor"]);
+                Console.WriteLine("VideoArchitecture - {0}", mo["VideoArchitecture"]);
+                Console.WriteLine("VideoMemoryType - {0}", mo["VideoMemoryType"]);*/
+            }
+            ManagementObjectSearcher gpuObject = new ("SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject mo in gpuObject.Get())
+            {
+                Gpu = mo["Name"].ToString();
+                /*Console.WriteLine("Name - {0}", mo["Name"]);
+                Console.WriteLine("Status - {0}", mo["Status"]);
+                Console.WriteLine("Caption - {0}", mo["Caption"]);
+                Console.WriteLine("DeviceID - {0}", mo["DeviceID"]);
+                Console.WriteLine("AdapterRAM - {0}", mo["AdapterRAM"]);
+                Console.WriteLine("AdapterDACType - {0}", mo["AdapterDACType"]);
+                Console.WriteLine("Monochrome - {0}", mo["Monochrome"]);
+                Console.WriteLine("InstalledDisplayDrivers - {0}", mo["InstalledDisplayDrivers"]);
+                Console.WriteLine("DriverVersion - {0}", mo["DriverVersion"]);
+                Console.WriteLine("VideoProcessor - {0}", mo["VideoProcessor"]);
+                Console.WriteLine("VideoArchitecture - {0}", mo["VideoArchitecture"]);
+                Console.WriteLine("VideoMemoryType - {0}", mo["VideoMemoryType"]);*/
             }
 
+            //Os = Environment.OSVersion.ToString();
+
+            
             // -- ici on simule le téléchargement des infos système
             // Get the WMI class
-            ManagementClass managementClass = new("Win32_Processor");
+            //ManagementClass managementClass = new("Win32_Processor");
 
             // Loop through the WMI class instances and print the processor information found
-            foreach (ManagementObject managementObject in managementClass.GetInstances())
+            /*foreach (ManagementObject managementObject in managementClass.GetInstances())
             {
                 Console.WriteLine("--- Processor information ---");
                 Console.WriteLine($"Name: {managementObject["Name"]}");
                 Console.WriteLine($"Architecture: {managementObject["Architecture"]}");
-            }
+            }*/
+            // Platform not supported: provide safe fallback values
+            //Cpu = DeviceInfo.Current.Model;
+            //Gpu = DeviceInfo.Current.Manufacturer;
+            
         }
+        #endregion
     }
 }
     
--- MauiAppPCCleaner_Formation/ViewModels/MainViewModel.cs
+++ MauiAppPCCleaner_Formation/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiAppPCCleaner_Formation.Infrastructure.System;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,8 @@ namespace MauiAppPCCleaner_Formation.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
+
+
         #region public properties 
         [ObservableProperty]
         public partial string Os { get; set; }
@@ -19,5 +23,30 @@ namespace MauiAppPCCleaner_Formation.ViewModels
         [ObservableProperty]
         public partial string Gpu { get; set; }
         #endregion
+
+        #region Constructeur
+        public MainViewModel()
+        {
+            InfoSystem info = new();
+            Os = info.Os ?? string.Empty;
+            Cpu = info.Cpu ?? string.Empty;
+            Gpu = info.Gpu ?? string.Empty;
+        }
+        #endregion
+
+
+        #region public methods 
+        /// <summary>
+        /// ouverture verrs le site internet du produit
+        /// </summary>
+        /// <returns></returns>
+        [RelayCommand]
+        public async Task ClickedInfo()
+        {
+            await Browser.Default.OpenAsync("", BrowserLaunchMode.SystemPreferred);
+        }
+
+        #endregion
+
     }
 }
